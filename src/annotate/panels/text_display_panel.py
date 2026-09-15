744095abd05a3e10c7f6be0b7f78e6f26a908e3c
esnyder <esnyder@cornell.edu>
2026-09-15 15:01:58 -0400

    restore working code

--- src/annotate/panels/text_display_panel.py
+++ src/annotate/panels/text_display_panel.py
@@ -38,7 +38,6 @@ class TextDisplayPanel(QWidget):
         layout.addWidget(self.timestamp_label)
         layout.addWidget(separator)
         layout.addWidget(self.cursor_mode_label)
-        layout.addWidget(self.annotation_prompt_label)
 
         # Another place to add more status/info text later
         self.info_label = QLabel("")
